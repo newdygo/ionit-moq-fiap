0400aa85f7bfc24f43dc047420f7d317010431c8
Diego Balduino <homertrix_x@hotmail.com>
2017-09-20 22:40:42 -0300

    Moq de controller

---
+++ MeuSite/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MeuSite.Models;
+
+namespace MeuSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository @object;
+
+        public HomeController(IRepository @object)
+        {
+            this.@object = @object;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = @object.List();
+
+            if (!list.Any())
+                return View("NoRecipes");
+
+            return View(list);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class Recipe
+    {
+        public int Id { get; set; }
+    }
+
+    public class Repository : IRepository
+    {
+        public List<Recipe> List()
+        {
+            return new List<Recipe>();
+        }
+    }
+
+    public interface IRepository
+    {
+        List<Recipe> List();
+    }
+}
---
+++ TesteFiap/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using MeuSite.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TesteFiap
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task dado_um_repository_sem_receitas_deve_retornar_uma_view_chamada_norecipesAsync()
+        {
+            var repository = new Moq.Mock<IRepository>();
+
+            repository.Setup(x => x.List()).Returns(new List<Recipe>());
+
+            var controller = new HomeController(repository.Object);
+            var result = await controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            Assert.Equal("NoRecipes", viewResult.ViewName);
+        }
+    }
+}
